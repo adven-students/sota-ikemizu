ff1aa6884826de6512df4528e12a14de36d930ba
adven-students <adven.students@gmail.com>
2025-11-27 15:31:36 +0900

    スコア制限時間の追加

---
+++ Basket/Assets/Script/GameDirector.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject timerText;
+    GameObject pointText;
+    float time=60.0f;
+    int point=0;
+    public void GetApple()
+    {
+        this.point+=100;
+    }
+    public void GetBomb()
+    {
+        this.point/=2;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.timerText=GameObject.Find("Time");
+        this.pointText=GameObject.Find("Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.time-=Time.deltaTime;
+        this.timerText.GetComponent<TextMeshProUGUI>().text=this.time.ToString("F1");
+        this.pointText.GetComponent<TextMeshProUGUI>().text=this.point.ToString()+" point";
+    }
+}
