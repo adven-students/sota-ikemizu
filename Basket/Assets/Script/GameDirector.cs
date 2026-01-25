095ab07d8159a0c847a49d1e2d773872c2dd8483
adven-students <adven.students@gmail.com>
2026-01-25 14:20:28 +0900

    Level Design
    レベルデザインをしました。ちょうどいい早さとスパンを見つけました。

--- Basket/Assets/Script/GameDirector.cs
+++ Basket/Assets/Script/GameDirector.cs
@@ -6,8 +6,9 @@ public class GameDirector : MonoBehaviour
 {
     GameObject timerText;
     GameObject pointText;
-    float time=60.0f;
+    float time=30.0f;
     public static int point=0;
+    GameObject generator;
     public void GetApple()
     {
         point+=100;
@@ -21,6 +22,7 @@ public class GameDirector : MonoBehaviour
     {
         this.timerText=GameObject.Find("Time");
         this.pointText=GameObject.Find("Point");
+        this.generator=GameObject.Find("ItemGenerator");
     }
 
     // Update is called once per frame
@@ -32,6 +34,27 @@ public class GameDirector : MonoBehaviour
         if(time<=0)
         {
             SceneManager.LoadScene("Result");
+        }
+        if(this.time<0)
+        {
+            this.time=0;
+            this.generator.GetComponent<ItemGenerator>().SetParameter(10000.0f,0,0);
+        }
+        else if (0<=this.time && this.time<5)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.2f,-0.08f,6);
+        }
+        else if (5<=this.time && this.time<10)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.3f,-0.07f,5);
+        }
+        else if (10<=this.time && this.time<20)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.4f,-0.06f,4);
+        }
+        else if (20<=this.time && this.time<30)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.5f,-0.05f,3);
         }
     }
     public int Geterpoint()
